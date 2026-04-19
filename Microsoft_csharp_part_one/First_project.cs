68667bf948bc57a6a9ced45b370ee79b754977ce
DennLis <DennisLisboa5@gmail.com>
2026-04-18 22:25:17 -0300

    git push

---
+++ Microsoft_csharp_part_one/First_project.cs
@@ -0,0 +1,18 @@
+//the first way to write a message
+Console.WriteLine("'Hello, World!' or 'Hello World!', it doesn't really matter!");
+
+//the other way to do it
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+//the difference here is that, one creates a full line when executed and the other just writes the content
+Console.WriteLine(" ");
+Console.WriteLine("Congratulations! ");
+Console.Write("You wrote your first lines of code.");
+
+//another example:
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
